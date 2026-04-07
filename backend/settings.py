6048a378037afa23820538920c869c9b2682e213
kowshikaravind4@gmail.com <kowshikaravind4@gmail.com>
2026-04-07 13:57:39 +0530

    Backend changed to postgreSQL

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,12 +81,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgresql://academic_performance_tracking_system_user:zvu8EcKKxAHgGJayw5B051ykEfVapp7u@dpg-d7aahc2dbo4c73ce6o0g-a.oregon-postgres.render.com/academic_performance_tracking_system')
 }
 
 
